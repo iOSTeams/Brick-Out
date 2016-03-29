3f6c77931fb621c54e8c681afb2b4eb0a82e68fb
Justin Lee <jlee402@mail.ccsf.edu>
2016-03-29 12:53:18 -0700

    Destroyed bricks, added win/lost screen

--- BrickOut/GameScene.swift
+++ BrickOut/GameScene.swift
@@ -9,7 +9,7 @@
 import SpriteKit
 import AVFoundation
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     
     var fingerIsOnPaddle = false
@@ -52,10 +52,17 @@ class GameScene: SKScene {
     let paddleCategoryName = "paddle"
     let brickCategoryName = "brick"
     
+    let ballCategory:UInt32 = 0x1 << 0
+    let paddleCategory:UInt32 = 0x1 << 1
+    let brickCategory:UInt32 = 0x1 << 2
+    let bottomCategory:UInt32 = 0x1 << 3
+    
     var backgroundMusicPlayer = AVAudioPlayer()
     
     override init(size: CGSize) {
         super.init(size:size)
+        
+        self.physicsWorld.contactDelegate = self
         
         //Set Background Music
         let bgMusicURL: NSURL = NSBundle.mainBundle().URLForResource("bgMusic", withExtension: "mp3")!
@@ -99,6 +106,69 @@ class GameScene: SKScene {
         paddle.physicsBody?.restitution = 0.1
         paddle.physicsBody?.dynamic = false
         
+        //Bottom
+        let bottomRect = CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, 1)
+        let bottom = SKNode()
+        bottom.physicsBody = SKPhysicsBody(edgeLoopFromRect: bottomRect)
+        
+        self.addChild(bottom)
+        
+        //Collision
+        bottom.physicsBody?.categoryBitMask = bottomCategory
+        ball.physicsBody?.categoryBitMask = ballCategory
+        paddle.physicsBody?.categoryBitMask = paddleCategory
+        ball.physicsBody?.contactTestBitMask = bottomCategory
+        ball.physicsBody?.contactTestBitMask = brickCategory
+        
+        
+        //Create bricks
+        let numberOfRows = 3
+        let numberOfBricks = 6
+        let brickWidth = SKSpriteNode(imageNamed: "brick").size.width
+        let padding:Float = 20
+        
+        //offset = frame - (brick * 6 + padding * 6) / 2
+        
+        //let offset:Float = Float(self.frame.size.width) - Float(brickWidth * 6) + Float(padding * 6  / 2)
+        let offset:Float = (Float(self.frame.size.width) - (Float(brickWidth) * Float(numberOfBricks) + padding * (Float(numberOfBricks) - 1 ) ) ) / 2
+        
+        //Create rows of bricks
+        
+        for index in 1 ... numberOfRows {
+            
+            var yOffset:CGFloat {
+                switch index {
+                case 1:
+                    return self.frame.height * 0.8
+                case 2:
+                    return self.frame.height * 0.6
+                case 3:
+                    return self.frame.height * 0.4
+                default:
+                    return 0
+                }
+            }
+            
+            for index in 1 ... numberOfBricks {
+                let brick = SKSpriteNode(imageNamed: "brick")
+                
+                let calc1:Float = Float(index) - 0.5
+                let calc2:Float = Float(index) - 1
+                
+                brick.position = CGPointMake(CGFloat(calc1 * Float(brick.frame.size.width) + calc2 * padding + offset), yOffset)
+                
+                brick.physicsBody = SKPhysicsBody(rectangleOfSize: brick.frame.size)
+                brick.physicsBody?.allowsRotation = false
+                brick.physicsBody?.friction = 0
+                brick.name = brickCategoryName
+                //brick.physicsBody?.dynamic = false
+                brick.physicsBody?.affectedByGravity = false
+                brick.physicsBody?.categoryBitMask = brickCategory
+                self.addChild(brick)
+            }
+            
+        }
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -121,15 +191,42 @@ class GameScene: SKScene {
             let paddle = self.childNodeWithName(paddleCategoryName) as! SKSpriteNode
             
             //Move paddle horizontally
-            var newPosX = paddle.position.x + (touchLocation.x - prevTouchLocation.x)
+            var newXPos = paddle.position.x + (touchLocation.x - prevTouchLocation.x)
             
+            newXPos = max(newXPos, paddle.size.width/2)
+            newXPos = min(newXPos, self.size.width - paddle.size.width/2)
             
-            
-            paddle.position = CGPointMake(newPosX, paddle.position.y)
+            paddle.position = CGPointMake(newXPos, paddle.position.y)
         }
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        fingerIsOnPaddle = false
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        var firstBody = SKPhysicsBody()
+        var secondBody = SKPhysicsBody()
+        
+        //Always make first body the smallest 
+        //Smallest will always be the ball
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        if firstBody.categoryBitMask == ballCategory && secondBody.categoryBitMask == bottomCategory {
+            print ("You lost")
+        }
+        
+        if firstBody.categoryBitMask == ballCategory && secondBody.categoryBitMask == brickCategory {
+            secondBody.node?.removeFromParent()
+            //Doesnt work if you turn dynamic off
+            print ("Destroyed")
+        }
         
     }
     
